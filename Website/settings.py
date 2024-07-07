9090a613d73abee79f4908004a907fbb00e8ffc7
Ritwik Gupta <ritwikg.0501@gmail.com>
2024-07-07 18:31:22 +0530

    Deployment 8

--- Website/settings.py
+++ Website/settings.py
@@ -36,8 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-
-    "whitenoise.runserver_nostatic",
     'django.contrib.staticfiles',
 
     # MY APPS
