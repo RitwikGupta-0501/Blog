fbfba738c38c783c235ae0fd8b82720185cd0d4f
Ritwik Gupta <ritwikg.0501@gmail.com>
2024-07-07 20:00:21 +0530

    Restricts Access to Admin Portal

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
 from .models import Blog
+from django.http import Http404
+from django.urls import reverse
+
+
+class RestrictStaffToAdminMiddleware(object):
+    """
+    A middleware that restricts staff members access to administration panels.
+    """
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+
+        if request.path.startswith(reverse("admin:index")):
+            if request.user.is_authenticated:
+                if not request.user.is_staff:
+                    raise Http404
+            else:
+                raise Http404
+
+        return self.get_response(request)
 
 # Register your models here.
 admin.site.register(Blog)
