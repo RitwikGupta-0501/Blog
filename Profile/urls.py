223b7160da6c96d9b27dc7dc2ebad7e1dfcf42d5
Ritwik Gupta <ritwikg.0501@gmail.com>
2024-10-08 16:29:24 +0530

    Clearing Branch for Beginning Frontend Work

--- Profile/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', profile_view, name='profile-view'),
-    path('create-blog/', create_blog, name='create-blog'),
-]
