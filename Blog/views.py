cd0abf41606022958c1c4de8da5450e07d2b7669
Ritwik Gupta <ritwikg.0501@gmail.com>
2025-04-01 01:19:50 +0530

    Removed Redundant imports

--- Blog/views.py
+++ Blog/views.py
@@ -1,9 +1,5 @@
-import django.db
 from django.contrib import messages
-from django.urls import reverse
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout
-from django.contrib.auth.models import User
 from django.http import Http404
 from django.shortcuts import get_object_or_404
 from .models import Blog
