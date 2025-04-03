537a9625057e1010963cd874c0217678a4585523
Ritwik Gupta <ritwikg.0501@gmail.com>
2025-04-04 01:37:03 +0530

    Implemented @unauthorized_user decorator to allow reusability

--- Profile/views.py
+++ Profile/views.py
@@ -5,14 +5,31 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 
+# Helper Functions
+def unauthenticated_user(view_func):
+    """
+    Decorator to check if user is authenticated.
+    If not, redirect to login page.
 
-# Create your views here.
+    :param view_func:
+    :return:
+    """
+    def wrapper(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("home")
+        else:
+            return view_func(request, *args, **kwargs)
+    return wrapper
+
+
+# Django Views
 @login_required
 def profile_view(request):
     blogs = request.user.blog_posts.all()
     return render(request, "profile.html", {'blogs': blogs})
 
 
+@unauthenticated_user
 def register_view(request):
     """
     Handles Registrations of the Web App.\n
@@ -22,22 +39,21 @@ def register_view(request):
     :param request:
     :returns HTML render:
     """
-    if not request.user.is_authenticated:
-        if request.method == "POST":
-            if request.POST["password1"] == request.POST["password2"]:
-                try:
-                    user = User.objects.create_user(username=request.POST["username"], password=request.POST["password1"])
-                    user.save()
-                    login(request, user)
-                    return redirect("home")
-                except IntegrityError as _:
-                    messages.error(request, "Username Already exists.")
-            else:
-                messages.error(request, "Passwords do not match.")
-        return render(request, "register.html")
-    return redirect("home")
+    if request.method == "POST":
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                user = User.objects.create_user(username=request.POST["username"], password=request.POST["password1"])
+                user.save()
+                login(request, user)
+                return redirect("home")
+            except IntegrityError as _:
+                messages.error(request, "Username Already exists.")
+        else:
+            messages.error(request, "Passwords do not match.")
+    return render(request, "register.html")
 
 
+@unauthenticated_user
 def login_view(request):
     """
     Handles Login of User.
@@ -47,18 +63,16 @@ def login_view(request):
     :param request:
     :return:
     """
-    if not request.user.is_authenticated:
-        if request.method == "POST":
-            username = request.POST["username"]
-            password = request.POST["password1"]
-            user = authenticate(request, username, password)
-            if user is not None:
-                login(request, user)
-                return redirect("home")
-            else:
-                messages.error(request, "Invalid Username or Password.")
-        return render(request, "login.html")
-    return redirect("home")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password1"]
+        user = authenticate(request, username, password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid Username or Password.")
+    return render(request, "login.html")
 
 
 @login_required
